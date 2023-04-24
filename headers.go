3ea4db4b7defd16fd636572dd871f3c2a79c538f
Dave Molk <davemolk@gmail.com>
2023-04-24 05:09:45 -0600

    add tests for headers, refactor

--- headers.go
+++ headers.go
@@ -2,14 +2,13 @@ package fuzzyHelpers
 
 import (
 	"math/rand"
-	"net/http"
 	"net/url"
 	"time"
 )
 
 type headers struct {
 	osys      string
-	HeaderMap map[string][]string
+	headerMap map[string][]string
 }
 
 type option func(*headers) error
@@ -21,7 +20,7 @@ func init() {
 func NewHeaders(opts ...option) (*headers, error) {
 	h := &headers{
 		osys:      "w",
-		HeaderMap: make(map[string][]string),
+		headerMap: make(map[string][]string),
 	}
 	for _, opt := range opts {
 		err := opt(h)
@@ -48,27 +47,14 @@ func WithOS(osys string) option {
 
 func WithURL(s string) option {
 	return func(h *headers) error {
-		u, err := url.Parse(s)
+		u, err := url.ParseRequestURI(s)
 		if err != nil {
 			// don't set Host on headers
 			return nil
 		}
-		h.HeaderMap["Host"] = []string{u.Host}
+		h.headerMap["Host"] = []string{u.Host}
 		return nil
 	}
-}
-
-func Headers() map[string][]string {
-	h, err := NewHeaders()
-	if err != nil {
-		panic(err)
-	}
-	if rand.Intn(2) == 1 {
-		h.firefox()
-	} else {
-		h.chrome()
-	}
-	return h.HeaderMap
 }
 
 func (h *headers) Headers() map[string][]string {
@@ -77,50 +63,54 @@ func (h *headers) Headers() map[string][]string {
 	} else {
 		h.chrome()
 	}
-	return h.HeaderMap
+	return h.headerMap
+}
+
+func Headers() map[string][]string {
+	h, err := NewHeaders()
+	if err != nil {
+		panic(err)
+	}
+	return h.Headers()
 }
 
 func (h *headers) firefox() {
 	uAgent := h.ffUA()
-	h.HeaderMap = http.Header{
-		"User-Agent":                {uAgent},
-		"Accept":                    {"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"},
-		"Accept-Language":           {"en-US,en;q=0.5"},
-		"DNT":                       {"1"},
-		"Connection":                {"keep-alive"},
-		"Upgrade-Insecure-Requests": {"1"},
-		"Sec-Fetch-Dest":            {"document"},
-		"Sec-Fetch-Mode":            {"navigate"},
-		"Sec-Fetch-Site":            {"none"},
-		"Sec-Fetch-User":            {"?1"},
-		"Sec-GCP":                   {"1"},
-	}
+	h.headerMap["User-Agent"] = []string{uAgent}
+	h.headerMap["Accept"] = []string{"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"}
+	h.headerMap["Accept-Language"] = []string{"en-US,en;q=0.5"}
+	h.headerMap["DNT"] = []string{"1"}
+	h.headerMap["Connection"] = []string{"keep-alive"}
+	h.headerMap["Upgrade-Insecure-Requests"] = []string{"1"}
+	h.headerMap["Sec-Fetch-Dest"] = []string{"document"}
+	h.headerMap["Sec-Fetch-Mode"] = []string{"navigate"}
+	h.headerMap["Sec-Fetch-Site"] = []string{"none"}
+	h.headerMap["Sec-Fetch-User"] = []string{"?1"}
+	h.headerMap["Sec-GCP"] = []string{"1"}
 }
 
 func (h *headers) chrome() {
 	uAgent := h.chromeUA()
-	h.HeaderMap = http.Header{
-		"Connection":                {"keep-alive"},
-		"Cache-Control":             {"max-age=0"},
-		"sec-ch-ua":                 {`" Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"`},
-		"sec-ch-ua-mobile":          {"?0"},
-		"Upgrade-Insecure-Requests": {"1"},
-		"User-Agent":                {uAgent},
-		"Accept":                    {"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"},
-		"Sec-Fetch-Site":            {"none"},
-		"Sec-Fetch-Mode":            {"navigate"},
-		"Sec-Fetch-User":            {"?1"},
-		"Sec-Fetch-Dest":            {"document"},
-		"Accept-Language":           {"en-US,en;q=0.5"},
-	}
+	h.headerMap["Connection"] = []string{"keep-alive"}
+	h.headerMap["Cache-Control"] = []string{"max-age=0"}
+	h.headerMap["sec-ch-ua"] = []string{`"Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"`}
+	h.headerMap["sec-ch-ua-mobile"] = []string{"?0"}
+	h.headerMap["Upgrade-Insecure-Requests"] = []string{"1"}
+	h.headerMap["User-Agent"] = []string{uAgent}
+	h.headerMap["Accept"] = []string{"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"}
+	h.headerMap["Sec-Fetch-Site"] = []string{"none"}
+	h.headerMap["Sec-Fetch-Mode"] = []string{"navigate"}
+	h.headerMap["Sec-Fetch-User"] = []string{"?1"}
+	h.headerMap["Sec-Fetch-Dest"] = []string{"document"}
+	h.headerMap["Accept-Language"] = []string{"en-US,en;q=0.5"}
 
 	switch h.osys {
 	case "m":
-		h.HeaderMap["sec-ch-ua-platform"] = []string{"Macintosh"}
+		h.headerMap["sec-ch-ua-platform"] = []string{"Macintosh"}
 	case "l":
-		h.HeaderMap["sec-ch-ua-platform"] = []string{"Linux"}
+		h.headerMap["sec-ch-ua-platform"] = []string{"Linux"}
 	default:
-		h.HeaderMap["sec-ch-ua-platform"] = []string{"Windows"}
+		h.headerMap["sec-ch-ua-platform"] = []string{"Windows"}
 	}
 }
 
@@ -129,27 +119,42 @@ func (h *headers) ffUA() string {
 	switch h.osys {
 	case "m":
 		userAgents = []string{
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:107.0) Gecko/20100101 Firefox/107.0",
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:106.0) Gecko/20100101 Firefox/106.0",
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:104.0) Gecko/20100101 Firefox/104.0",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 13.13.1; rv:110.0) Gecko/20100101 Firefox/110.0",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 13.13; rv:108.0) Gecko/20100101 Firefox/108.0",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 13.13; rv:106.0) Gecko/20100101 Firefox/106.0",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 11.7.6; rv:108.0) Gecko/20100101 Firefox/108.0",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 11.1; rv:108.0) Gecko/20100101 Firefox/108.0",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 11.1; rv:110.0) Gecko/20100101 Firefox/110.0",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15.7; rv:106.0) Gecko/20100101 Firefox/106.0",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 12.6.5; rv:110.0) Gecko/20100101 Firefox/110.0",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 12.1; rv:104.0) Gecko/20100101 Firefox/104.0",
 			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0",
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:101.0) Gecko/20100101 Firefox/101.0",
 		}
 	case "l":
 		userAgents = []string{
-			"Mozilla/5.0 (X11; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0 ",
-			"Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0 ",
-			"Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0 ",
-			"Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0 ",
-			"Mozilla/5.0 (X11; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0 ",
+			"Mozilla/5.0 (X11; Linux x86_64; rv:110.0) Gecko/20100101 Firefox/110.0",
+			"Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/109.0",
+			"Mozilla/5.0 (X11; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0",
+			"Mozilla/5.0 (X11; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0",
+			"Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0",
+			"Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0",
+			"Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0",
+			"Mozilla/5.0 (X11; Linux x86_64; rv:103.0) Gecko/20100101 Firefox/103.0",
+			"Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0",
+			"Mozilla/5.0 (X11; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0",
 		}
 	default:
 		userAgents = []string{
-			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:110.0) Gecko/20100101 Firefox/110.0",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0",
 			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:108.0) Gecko/20100101 Firefox/108.0",
-			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:107.0) Gecko/20100101 Firefox/107.0",
 			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0",
 			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:104.0) Gecko/20100101 Firefox/104.0",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:102.0) Gecko/20100101 Firefox/102.0",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:99.0) Gecko/20100101 Firefox/99.0",
 		}
 	}
 	random := rand.Intn(len(userAgents))
@@ -166,12 +171,22 @@ func (h *headers) chromeUA() string {
 			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
 			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4692.56 Safari/537.36",
 			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4889.0 Safari/537.36",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 13_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 11_7_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 12_6_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
 		}
 	case "l":
 		userAgents = []string{
 			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36",
 			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
-			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.137 Safari/537.36",
 			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4692.56 Safari/537.36",
 			"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4889.0 Safari/537.36",
 		}
@@ -182,6 +197,11 @@ func (h *headers) chromeUA() string {
 			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.54 Safari/537.36",
 			"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36",
 			"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+			"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.5615.137 Safari/537.36",
 		}
 	}
 	random := rand.Intn(len(userAgents))
---
+++ headers_test.go
@@ -0,0 +1,174 @@
+package fuzzyHelpers
+
+import (
+	"testing"
+)
+
+func TestChrome(t *testing.T) {
+	t.Parallel()
+	h, err := NewHeaders()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("no headers at initialization", func(t *testing.T) {
+		want := 0
+		got := len(h.headerMap)
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+	t.Run("chrome() provides correct number of chrome headers", func(t *testing.T) {
+		want := 13
+		h.chrome()
+		got := len(h.headerMap)
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+}
+
+func TestFirefox(t *testing.T) {
+	t.Parallel()
+	h, err := NewHeaders()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("no headers at initialization", func(t *testing.T) {
+		want := 0
+		got := len(h.headerMap)
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+	t.Run("firefox() provides correct number of headers", func(t *testing.T) {
+		want := 11
+		h.firefox()
+		got := len(h.headerMap)
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
+	})
+}
+
+func TestWithURL(t *testing.T) {
+	t.Parallel()
+	h, err := NewHeaders(
+		WithURL("https://example.com/foo"),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	foo := h.Headers()
+	want := "example.com"
+	got := foo["Host"][0]
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func TestWithURLNoURLIfErr(t *testing.T) {
+	t.Parallel()
+	h, err := NewHeaders(
+		WithURL("kljkl"),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	foo := h.Headers()
+	if v, ok := foo["Host"]; ok {
+		t.Errorf("wanted no host header, got %v", v)
+	}
+}
+
+func TestWithOSInHeader(t *testing.T) {
+	t.Parallel()
+	h, err := NewHeaders(
+		WithOS("m"),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// check chrome for sec-ch-ua-platform header
+	h.chrome()
+	want := "Macintosh"
+	got := h.headerMap["sec-ch-ua-platform"][0]
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func TestWithOSDefaultWindows(t *testing.T) {
+	t.Parallel()
+	h, err := NewHeaders(
+		WithOS("foo"),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// check chrome for sec-ch-ua-platform header
+	h.chrome()
+	want := "Windows"
+	got := h.headerMap["sec-ch-ua-platform"][0]
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
+func TestWithOSInUA(t *testing.T) {
+	t.Parallel()
+	h, err := NewHeaders(
+		WithOS("m"),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	h.chrome()
+	ua := []string{
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4692.56 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4889.0 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 13_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 11_7_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 12_6_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36",
+		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+	}
+	if !assertCorrectUA(t, h.headerMap["User-Agent"][0], ua) {
+		t.Errorf("wanted %s to be a Macintosh ua", h.headerMap["User-Agent"][0])
+	}
+
+}
+
+func assertCorrectUA(t *testing.T, ua string, possible []string) bool {
+	t.Helper()
+	for _, v := range possible {
+		if ua == v {
+			return true
+		}
+	}
+	return false
+}
+
+func TestHeaders(t *testing.T) {
+	t.Parallel()
+	headers := Headers()
+	t.Run("success", func(t *testing.T) {
+		if len(headers) == 0 {
+			t.Errorf("want headers, got none")
+		}
+	})
+	t.Run("no host set", func(t *testing.T) {
+		if v, ok := headers["Host"]; ok {
+			t.Errorf("wanted no Host header, got %v", v)
+		}
+	})
+	t.Run("correct number of headers", func(t *testing.T) {
+		if len(headers) != 11 && len(headers) != 13 {
+			t.Errorf("number of headers was %d, wanted 11 or 13", len(headers))
+		}
+	})
+}
